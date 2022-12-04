2c1500ca10cce8435ae0af0ec039a316cca2a401
Beef <beefers@riseup.net>
2022-12-04 14:56:43 +0000

    [2022 > Day04] Clean up solution

--- 2022/src/day04/index.ts
+++ 2022/src/day04/index.ts
@@ -1,32 +1,27 @@
 import run from "aocrunner";
 
-/*
-    Not the most proud of what I wrote here today.
-    It works, though...
-    (I fear for the later days)
-*/
-
-// TODO: clean
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from#sequence_generator_range
-const range = (start: number, stop: number, step: number) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
-const checkArray = (source: Array<any>, target: Array<any>) => target.every(v => source.includes(v));
 const parseInput = (rawInput: string) => rawInput.split("\n");
+
+function getSects(pair: string) {
+    const splitPairs = pair.split(",");
+    const pair1 = splitPairs[0].split("-").map(Number);
+    const pair2 = splitPairs[1].split("-").map(Number)
+
+    return {
+        min1: pair1[0],
+        max1: pair1[1],
+        min2: pair2[0],
+        max2: pair2[1],
+    }
+}
 
 const part1 = (rawInput: string) => {
     const pairs = parseInput(rawInput);
     let totalPairs = 0;
 
     for (let pair of pairs) {
-        const splitPairs = pair.split(",");
-        const firstPair = splitPairs[0].split("-").map(Number);
-        const secondPair = splitPairs[1].split("-").map(Number);
-        const firstRange = range(firstPair[0], firstPair[1], 1);
-        const secondRange = range(secondPair[0], secondPair[1], 1);
-
-        if (checkArray(firstRange, secondRange) || checkArray(secondRange, firstRange)) {
-            totalPairs++;
-        }
+        const { min1, max1, min2, max2 } = getSects(pair);
+        if ((min1 >= min2 && max1 <= max2) || (min2 >= min1 && max2 <= max1)) totalPairs++;
     }
 
     return totalPairs;
@@ -37,23 +32,8 @@ const part2 = (rawInput: string) => {
     let totalPairs = 0;
 
     for (let pair of pairs) {
-        const splitPairs = pair.split(",");
-        const firstPair = splitPairs[0].split("-").map(Number);
-        const secondPair = splitPairs[1].split("-").map(Number);
-        const firstRange = range(firstPair[0], firstPair[1], 1);
-        const secondRange = range(secondPair[0], secondPair[1], 1);
-        const biggest = firstRange.length > secondRange.length ? firstRange : secondRange;
-        const smallest = biggest === firstRange ? secondRange : firstRange;
-        let didFindPair = false;
-
-        for (let i of smallest) {
-            if (didFindPair) continue;
-
-            if (biggest.includes(i)) { 
-                totalPairs++;
-                didFindPair = true;
-            }
-        }
+        const { min1, max1, min2, max2 } = getSects(pair);
+        if (min1 <= max2 && min2 <= max1) totalPairs++;
     }
 
     return totalPairs;
